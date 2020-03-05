365c7ea31aac85d886c57db68ab3fb48dc1e95ca
Katerina <katerinalupacheva@gmail.com>
2020-03-05 15:47:42 +0300

    render array

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 
 const bands = [
   {
+    id: 1,
     name: 'Nightwish',
     albums: 9,
     members: 6,
     formed_in: 1996,
   },
   {
+    id: 2,
     name: 'Metallica',
     albums: 10,
     members: 4,
     formed_in: 1981,
   },
   {
+    id: 3,
     name: 'Nirvana',
     albums: 3,
     members: 3,
@@ -23,7 +26,18 @@ const bands = [
 ];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {bands.map(band => (
+        <div key={band.id} style={{ margin: '30px' }}>
+          <div>{`Band: ${band.name}`}</div>
+          <div>{`Albums: ${band.albums}`}</div>
+          <div>{`Members: ${band.members}`}</div>
+          <div>{`Year of founding: ${band.formed_in}`}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
